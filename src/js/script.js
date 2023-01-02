27f5e6f0119108309846b1f5ec9e2071336f4663
Michal Osinski <michal.osinski94@gmail.com>
2023-01-02 21:07:35 +0100

    final initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -80,17 +80,19 @@
     initAccordion() {
       const thisProduct = this;
       /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = ???;
-
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       /* START: add event listener to clickable trigger on event click */
-      clickableTrigger.addEventListener('click', function (event) {
+      clickableTrigger.addEventListener('click', function(event) {
         /* prevent default action for event */
-
+        event.preventDefault();
         /* find active product (product that has active class) */
-
+        const activeProduct = thisProduct.element.querySelector(classNames.menuProduct.wrapperActive);
         /* if there is active product and it's not thisProduct.element, remove class active from it */
-
+        if(activeProduct && activeProduct !== thisProduct.element){
+          activeProduct.classList.remove('active');
+        }
         /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle('active');
       });
     }
   }
