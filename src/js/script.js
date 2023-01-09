4349055704290b34c8ffd569e11c50a1f9e3e04e
Michal Osinski <michal.osinski94@gmail.com>
2023-01-09 18:11:24 +0100

    fix initActions

--- src/js/script.js
+++ src/js/script.js
@@ -191,10 +191,12 @@
     constructor(element) {
       const thisWidget = this;
 
+      thisWidget.getElements(element);
+      thisWidget.setValue(thisWidget.input.value);
+      thisWidget.initActions();
+
       console.log('AmountWidget:', thisWidget);
       console.log('constructor arguments:', element);
-
-      thisWidget.getElements(element);
     }
 
     getElements(element) {
@@ -216,15 +218,36 @@
 
       if (thisWidget.value !== newValue && !isNaN(newValue)) {
         thisWidget.value = newValue;
+        //console.log('setValue:', newValue);
 
         if (thisWidget.input.newValue < 0) {
           thisWidget.linkDecrease = 0;
         }
+
       }
 
     }
-  }
 
+    initActions() {
+      const thisWidget = this;
+
+      thisWidget.input.addEventListener('change', function() {
+        thisWidget.setValue(thisWidget.input.value);
+      });
+
+      thisWidget.linkDecrease.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        thisWidget.setValue(thisWidget.value -1);
+      });
+
+      thisWidget.linkIncrease.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        thisWidget.setValue(thisWidget.value +1);
+      });
+    }
+  }
 
   const app = {
     initMenu: function () {
