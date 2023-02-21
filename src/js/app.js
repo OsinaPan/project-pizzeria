12f7c05b8323001312163e3df1e554686503c529
Michal Osinski <michal.osinski94@gmail.com>
2023-02-21 18:30:59 +0100

    added thiapp cart

--- src/js/app.js
+++ src/js/app.js
@@ -1,57 +1,63 @@
-import{settings, select} from './settings.gs';
+import { settings, select } from './settings.gs';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
-  const app = {
-    initMenu: function () {
-      const thisApp = this;
-      //console.log('thisApp.data:', thisApp.data);
+const app = {
+  initMenu: function () {
+    const thisApp = this;
+    //console.log('thisApp.data:', thisApp.data);
 
-      for (let productData in thisApp.data.products) {
-        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
-      }
-    },
+    for (let productData in thisApp.data.products) {
+      new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
+    }
+  },
 
-    initData: function () {
-      const thisApp = this;
+  initData: function () {
+    const thisApp = this;
 
-      thisApp.data = {};
+    thisApp.data = {};
 
-      const url = settings.db.url + '/' + settings.db.products;
+    const url = settings.db.url + '/' + settings.db.products;
 
-      fetch(url)
-        .then(function(rawResponse){
-          return rawResponse.json();
-        })
-        .then(function (parsedResponse) {
-          console.log('parsedResponse', parsedResponse);
-          /* save paresedResponse as thisApp.data.products */
-          thisApp.data.products = parsedResponse;
-          /* execute initMenu method */
-          thisApp.initMenu();
-        });
+    fetch(url)
+      .then(function (rawResponse) {
+        return rawResponse.json();
+      })
+      .then(function (parsedResponse) {
+        console.log('parsedResponse', parsedResponse);
+        /* save paresedResponse as thisApp.data.products */
+        thisApp.data.products = parsedResponse;
+        /* execute initMenu method */
+        thisApp.initMenu();
+      });
 
-      console.log('thisAdd.data', JSON.stringify(thisApp.data));
-    },
+    console.log('thisAdd.data', JSON.stringify(thisApp.data));
+  },
 
-    initCart: function () {
-      const thisApp = this;
+  initCart: function () {
+    const thisApp = this;
 
-      const cartElem = document.querySelector(select.containerOf.cart);
-      thisApp.cart = new Cart(cartElem);
-    },
+    const cartElem = document.querySelector(select.containerOf.cart);
+    thisApp.cart = new Cart(cartElem);
 
-    init: function () {
-      const thisApp = this;
-      //console.log('*** App starting ***');
-      //console.log('thisApp:', thisApp);
-      //console.log('classNames:', classNames);
-      //console.log('settings:', settings);
-      //console.log('templates:', templates);
+    thisApp.productList = document.querySelector(select.containerOf.menu);
 
-      thisApp.initData();
-      thisApp.initCart();
-    },
-  };
+    thisApp.productList.addEventListener('add-to-cart', function(event){
+      app.cart.add(event.detail.product);
+    });
+  },
 
-  app.init();
+  init: function () {
+    const thisApp = this;
+    //console.log('*** App starting ***');
+    //console.log('thisApp:', thisApp);
+    //console.log('classNames:', classNames);
+    //console.log('settings:', settings);
+    //console.log('templates:', templates);
+
+    thisApp.initData();
+    thisApp.initCart();
+  },
+};
+
+app.init();
