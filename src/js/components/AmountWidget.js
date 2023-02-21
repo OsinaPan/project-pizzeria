98dfa77051a31ede3b46f002df8467880273a4f3
Michal Osinski <michal.osinski94@gmail.com>
2023-02-21 18:23:37 +0100

    fixed tabs and spaces

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -1,64 +1,64 @@
 class AmountWidget {
-    constructor(element) {
-      const thisWidget = this;
+  constructor(element) {
+    const thisWidget = this;
 
-      thisWidget.getElements(element);
-      thisWidget.setValue(thisWidget.input.value);
-      thisWidget.initActions();
+    thisWidget.getElements(element);
+    thisWidget.setValue(thisWidget.input.value);
+    thisWidget.initActions();
 
-      console.log('AmountWidget:', thisWidget);
-      console.log('constructor arguments:', element);
-    }
-
-    getElements(element) {
-      const thisWidget = this;
-
-      thisWidget.element = element;
-      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
-      thisWidget.setValue(settings.amountWidget.defaultValue);
-    }
-
-    setValue(value) {
-      const thisWidget = this;
-      const newValue = parseInt(value);
-
-      /* TODO: Add Validation */
-      if (thisWidget.value !== newValue && !isNaN(newValue)) {
-        thisWidget.value = newValue;
-      }
-
-      thisWidget.input.value = thisWidget.value;
-      thisWidget.announce();
-    }
-
-    announce() {
-      const thisWidget = this;
-      const event = new CustomEvent('updated', {
-        bubbles: true
-      });
-      thisWidget.element.dispatchEvent(event);
-    }
-
-    initActions() {
-      const thisWidget = this;
-      thisWidget.setValue(thisWidget.input.value);
-
-      thisWidget.input.addEventListener('change', function () {
-        thisWidget.setValue(thisWidget.input.value);
-      });
-
-      thisWidget.linkDecrease.addEventListener('click', function (event) {
-        event.preventDefault();
-
-        thisWidget.setValue(thisWidget.value - 1);
-      });
-
-      thisWidget.linkIncrease.addEventListener('click', function (event) {
-        event.preventDefault();
-
-        thisWidget.setValue(thisWidget.value + 1);
-      });
-    }
+    console.log('AmountWidget:', thisWidget);
+    console.log('constructor arguments:', element);
   }
+
+  getElements(element) {
+    const thisWidget = this;
+
+    thisWidget.element = element;
+    thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+    thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+    thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    thisWidget.setValue(settings.amountWidget.defaultValue);
+  }
+
+  setValue(value) {
+    const thisWidget = this;
+    const newValue = parseInt(value);
+
+    /* TODO: Add Validation */
+    if (thisWidget.value !== newValue && !isNaN(newValue)) {
+      thisWidget.value = newValue;
+    }
+
+    thisWidget.input.value = thisWidget.value;
+    thisWidget.announce();
+  }
+
+  announce() {
+    const thisWidget = this;
+    const event = new CustomEvent('updated', {
+      bubbles: true
+    });
+    thisWidget.element.dispatchEvent(event);
+  }
+
+  initActions() {
+    const thisWidget = this;
+    thisWidget.setValue(thisWidget.input.value);
+
+    thisWidget.input.addEventListener('change', function () {
+      thisWidget.setValue(thisWidget.input.value);
+    });
+
+    thisWidget.linkDecrease.addEventListener('click', function (event) {
+      event.preventDefault();
+
+      thisWidget.setValue(thisWidget.value - 1);
+    });
+
+    thisWidget.linkIncrease.addEventListener('click', function (event) {
+      event.preventDefault();
+
+      thisWidget.setValue(thisWidget.value + 1);
+    });
+  }
+}
